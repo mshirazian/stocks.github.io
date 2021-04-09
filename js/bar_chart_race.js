6a2bea3e899d47abd4dc980430245bdff2f50c26
mshirazian <mshirazian@gmail.com>
2021-04-09 11:39:57 -0700

    Add files via upload

---
+++ js/bar_chart_race.js
@@ -0,0 +1,241 @@
+//Bar Chart Race code
+
+// set the dimensions and margins of the graph
+var margin2 = {top: 50, right: 0, bottom: 50, left: 0},
+    width2 = 1000 - margin2.left - margin2.right,
+    height2 = 400 - margin2.top - margin2.bottom;
+
+// append the svg object to the body of the page
+var svg2 = d3.select("#bar_chart_race")
+  .append("svg")
+    .attr("viewBox", '0 0 1050 400') // to make svg responsive
+  .append("g")
+    .attr("transform",
+          "translate(" + margin2.left + "," + margin2.top + ")");
+
+// to convert dates
+var dateFormatter2 = d3.timeFormat("%Y-%m-%d");
+
+// length of animation
+var tickDuration = 400;
+
+//How many stocks to show
+var top_n = 10;
+
+//space between bars
+let barPadding = (height2-(margin2.bottom+margin2.top))/(top_n*5);
+
+// title and subtitle
+let title = svg2.append('text')
+     .attr('class', 'title')
+     .attr('y', 0)
+     .html('10 Most Popular Stocks on Robinhood');
+let subTitle = svg2.append("text")
+     .attr("class", "subTitle")
+     .attr("y", 27)
+     .html("Number of shares held 2020-01-16 to 2020-07-06");
+
+// Get first day in data
+let start_date = "2020-01-16";
+
+// Get all buttons as variables
+var playButton = d3.select("#play-button");
+var pauseButton = d3.select("#pause-button");
+var resetButton = d3.select("#reset-button");
+
+// Read data and create plot
+d3.csv('https://raw.githubusercontent.com/jonathangiguere/Robinhood_Investment_Web_App/master/data/top_50_popularity.csv', function (data) { 
+/*d3.csv('top_50_popularity.csv', function (data) {*/
+
+    // Format data
+    data.forEach(d => {
+        d.users_holding = +d.users_holding,
+        d.timestamp_date = d3.timeParse("%Y-%m-%d")(d.timestamp), //Get date as date object
+        d.colour = '#74ec74'
+    });
+
+    // filter data for first day of data
+    let daySlice = data.filter(d => d.timestamp == start_date)
+        .sort((a,b) => b.users_holding - a.users_holding)
+        .slice(0, top_n);
+
+        daySlice.forEach((d,i) => d.rank = i); // rank top 10 by popularity
+
+    // Scales and axes
+    let x2 = d3.scaleLinear()
+        .domain([0, d3.max(daySlice, d => d.users_holding)])
+        .range([margin.left, width + 130]);
+
+    let y2 = d3.scaleLinear()
+        .domain([top_n, 0])
+        .range([height-margin.bottom, margin.top]);
+
+    let xAxis2 = d3.axisTop()
+        .scale(x2)
+        .ticks(width > 500 ? 5:2)
+        .tickSize(-(height-margin.top-margin.bottom))
+        .tickFormat(d => d3.format(',')(d));
+
+    svg2.append('g')
+       .attr('class', 'axis xAxis')
+       .attr('transform', `translate(0, ${margin.top})`)
+       .call(xAxis2)
+       .selectAll('.tick line')
+       .classed('origin', d => d == 0);
+
+    // Add bars
+    svg2.selectAll('rect.bar')
+        .data(daySlice, d => d.Ticker)
+        .enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('x', x2(0)+1)
+        .attr('width', d => x2(d.users_holding)-x2(0)-1)
+        .attr('y', d => y2(d.rank)+5)
+        .attr('height', y2(1)-y2(0)-barPadding)
+        .style('fill', d => d.colour);
+
+    // Ticker label for bars
+    svg2.selectAll('text.label')
+        .data(daySlice, d => d.Ticker)
+        .enter()
+        .append('text')
+        .attr('class', 'label')
+        .attr('x', d => x2(d.users_holding)-8)
+        .attr('y', d => y2(d.rank)+5+((y2(1)-y2(0))/2)+3)
+        .style('text-anchor', 'end')
+        .html(d => d.Ticker);
+
+    // Year label
+    let dayText = svg2.append('text')
+      .attr('class', 'yearText')
+      .attr('x', width2-190)
+      .attr('y', height2-50)
+      .style('text-anchor', 'end')
+      .html(start_date);
+
+    // ----------------------------------------------------------------------------------------
+
+    // call function when play button is clicked
+    playButton
+        .on("click", toggleAnimating);
+
+    var animateTimer;
+
+    // function that does animation
+    // Basically updates the start date each iteration to get that day's data
+    function toggleAnimating(){
+
+          animateTimer = setInterval(function(){
+
+              // filter data for next day
+              daySlice = data.filter(d => d.timestamp == start_date)
+                .sort((a,b) => b.users_holding - a.users_holding)
+                .slice(0,top_n);
+
+              daySlice.forEach((d,i) => d.rank = i);
+
+              // Update x axis
+              x2.domain([0, d3.max(daySlice, d => d.users_holding)]);
+
+              svg2.select('.xAxis')
+                .transition()
+                .duration(tickDuration)
+                .ease(d3.easeLinear)
+                .call(xAxis2);
+
+              // Update bars
+              let bars = svg2.selectAll('.bar').data(daySlice, d => d.Ticker);
+
+               bars
+                .enter()
+                .append('rect')
+                .attr('class', d => `bar ${d.Ticker.replace(/\s/g,'_')}`)
+                .attr('x', x2(0)+1)
+                .attr( 'width', d => x2(d.users_holding)-x2(0)-1)
+                .attr('y', d => y2(top_n+1)+5)
+                .attr('height', y2(1)-y2(0)-barPadding)
+                .style('fill', d => d.colour)
+                .transition()
+                  .duration(tickDuration)
+                  .ease(d3.easeLinear)
+                  .attr('y', d => y2(d.rank)+5);
+
+               bars
+                .transition()
+                  .duration(tickDuration)
+                  .ease(d3.easeLinear)
+                  .attr('width', d => x2(d.users_holding)-x2(0)-1)
+                  .attr('y', d => y2(d.rank)+5);
+
+               bars
+                .exit()
+                .transition()
+                  .duration(tickDuration)
+                  .ease(d3.easeLinear)
+                  .attr('width', d => x2(d.users_holding)-x2(0)-1)
+                  .attr('y', d => y2(top_n+1)+10000) //add 10,000 to get bar out of svg canvas
+                  .remove();
+
+              // Update labels on bars
+              let labels = svg2.selectAll('.label')
+                    .data(daySlice, d => d.Ticker);
+
+               labels
+                .enter()
+                .append('text')
+                .attr('class', 'label')
+                .attr('x', d => x2(d.users_holding)-8)
+                .attr('y', d => y2(top_n+1)+5+((y2(1)-y2(0))/2))
+                .style('text-anchor', 'end')
+                .html(d => d.Ticker)
+                .transition()
+                  .duration(tickDuration)
+                  .ease(d3.easeLinear)
+                  .attr('y', d => y2(d.rank)+5+((y2(1)-y2(0))/2)+3);
+
+               labels
+                  .transition()
+                  .duration(tickDuration)
+                    .ease(d3.easeLinear)
+                    .attr('x', d => x2(d.users_holding)-8)
+                    .attr('y', d => y2(d.rank)+5+((y2(1)-y2(0))/2)+3);
+
+               labels
+                  .exit()
+                  .transition()
+                    .duration(tickDuration)
+                    .ease(d3.easeLinear)
+                    .attr('x', d => x2(d.users_holding)-8)
+                    .attr('y', d => y2(top_n+1)+10000) // add 10,000 to get label out of svg canvas
+                    .remove();
+
+              // Update date text in bottom right
+              dayText.html(start_date);
+
+              // clear interval on pause button click
+              // this pauses the animation
+              pauseButton
+                .on("click", function() {
+                  clearInterval(animateTimer);
+                });
+
+              // Reset date to first date when reset button is clicked
+              resetButton
+                .on("click", function() {
+                  start_date = "2020-01-16";
+                });
+
+            //End the ticker with specified date
+            if(start_date == "2020-07-06") clearInterval(animateTimer);
+
+              // This is where the date string gets converted to a date object
+              // Then it gets incremented and turned back into a string to filter
+              // data for the next day
+              var start_date_date = d3.timeParse("%Y-%m-%d")(start_date);
+              var tomorrow = start_date_date;
+              tomorrow.setDate(start_date_date.getDate() + 1);
+              start_date = dateFormatter2(tomorrow);
+              },tickDuration);
+            }
+          })
---
+++ js/line_chart.js
@@ -0,0 +1,324 @@
+//Dynamic line chart code
+
+// set the dimensions and margins of the graph
+var margin = {top: 50, right: 200, bottom: 50, left: 100},
+    width = 900 - margin.left - 100,
+    height = 400 - margin.top - margin.bottom;
+
+// Create bisector to be used for tool tip mouse over
+var bisectDate = d3.bisector(function(d) { return d.begins_at; }).left;
+
+// Create formatting rules for tool tip
+var formatValue = d3.format("$,.2f");
+var dateFormatter = d3.timeFormat("%m/%d/%y");
+
+// append the svg object to the body of the page
+var svg = d3.select("#line_chart_dynamic")
+  .append("svg")
+    .attr("viewBox", '0 0 1000 400') // to make svg responsive
+    .style("padding-bottom", "40")
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+//Read the data
+d3.csv("https://raw.githubusercontent.com/jonathangiguere/Robinhood_Investment_Web_App/master/data/top_50.csv", function(data) {
+
+    // format the data
+    data.forEach(function(d) {
+            d.begins_at = d3.timeParse("%Y-%m-%d")(d.begins_at);
+            d.volume = +d.volume;
+            d.close_price = +d.close_price;
+    });
+
+    // List of groups by stock symbol
+    var allGroup = d3.map(data, function(d){return(d.symbol)}).keys()
+
+    // add the options to the ticker dropdown
+    d3.select("#selectButton")
+      .selectAll('myOptions')
+        .data(allGroup)
+        .enter()
+        .append('option')
+      .text(function (d) { return d; }) // text showed in the menu
+      .attr("value", function (d) { return d; }) // corresponding value returned by the button
+
+    // Add X axis --> it is a date format
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.begins_at; }))
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+    // Filter for Ford Ticker which is first up
+    var dataFilter = data.filter(function(d){return d.symbol=='F'})
+
+    // Add Y axis with selected group to get y axis right
+    var y = d3.scaleLinear()
+      .domain([d3.min(dataFilter, function(d) { return d.close_price; }), d3.max(dataFilter, function(d) { return d.close_price; })])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .attr("class", "y_axis")
+      .call(d3.axisLeft(y)
+        .tickFormat(d3.format("$.2f")));
+
+    // Initialize the Y axis for the bar chart
+    var y_bar = d3.scaleLinear()
+      .range([ height,0]);
+    var yAxis = svg.append("g")
+      .attr("class", "myYaxis");
+
+    // Initialize the X axis for the bar chart
+    var x_bar = d3.scaleBand()
+      .range([ 0, width ])
+      .padding(0.5);
+    var xAxis = svg.append("g")
+      .attr("transform", "translate(0," + height + ")");
+
+    // Add Y axis for bar chart
+    y_bar
+        .domain([0, d3.max(data, function(d) { return d.volume }) ]);
+    yAxis
+        .attr("transform", "translate(" + width + ",0)")
+        .call(d3.axisRight(y_bar)
+            .tickFormat(d3.format(".2s")));
+
+    // text label for the y axis
+      svg.append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 25 - margin.left)
+          .attr("x", 0 - (height / 2))
+          .attr("dy", "1em")
+          .style("text-anchor", "middle")
+          .style("fill", "white")
+          .style("font-weight", "500")
+          .style("font-size", "14px")
+          .text("Market Close Price");
+
+    // text label for the 2nd y axis
+      svg.append("text")
+          .attr("transform", "rotate(90)")
+          .attr("y", -775)
+          .attr("x", (height / 2))
+          .attr("dy", "1em")
+          .style("text-anchor", "middle")
+          .style("fill", "white")
+          .style("font-weight", "500")
+          .style("font-size", "14px")
+          .text("Trading Volume");
+
+    // add X axis for bar chart
+    x_bar
+        .domain(data.map(function(d) { return dateFormatter(d.begins_at); }))
+    xAxis
+        .transition()
+        .duration(1000)
+        .attr('id', 'x_bar_axis')
+        .call(d3.axisTop(x_bar));
+
+    // text label for the x axis
+    svg.append("text")
+        .attr("y", height - (margin.top - 90))
+        .attr("x", (width / 2))
+        .style("text-anchor", "middle")
+        .style("fill", "white")
+        .style("font-weight", "500")
+        .style("font-size", "14px")
+        .text("Day");
+
+    // add title
+    svg.append('text')
+     .attr('class', 'title')
+     .attr('y', -30)
+     .attr('x', -100)
+     .html('Close Price and Volume for Selected Ticker');
+
+
+    // variable u: map data to existing bars
+    var u = svg.selectAll("rect")
+      .data(dataFilter)
+
+    // update bars
+    u
+      .enter()
+      .append("rect")
+      .merge(u)
+        .attr("x", function(d) { return x_bar(dateFormatter(d.begins_at)); })
+        .attr("y", function(d) { return y_bar(d.volume); })
+        .attr("width", x_bar.bandwidth())
+        .attr("height", function(d) { return height - y_bar(d.volume); })
+        .attr("fill", "#777777")
+
+    // Initialize line with first group of the list
+    var line = svg
+      .append('g')
+      .append("path")
+        .datum(data.filter(function(d){return d.symbol==allGroup[0]}))
+        .attr("d", d3.line()
+          .x(function(d) { return x(d.begins_at) })
+          .y(function(d) { return y(d.close_price) })
+        )
+        .attr("stroke", '#74ec74')
+        .style("stroke-width", 2)
+        .style("fill", "none")
+
+    // appending group element to SVG canvas
+    var focus = svg.append("g")
+        .attr("class", "focus")
+        .style("display", "none");
+
+    // Add a circle to focus group
+     focus.append("circle")
+        .attr("r", 3);
+
+    // Add actual tool tip box to focus group
+    focus.append("rect")
+        .attr("class", "tooltip")
+        .attr("width", 160)
+        .attr("height", 50)
+        .attr("x", 10)
+        .attr("y", -22)
+        .attr("rx", 4)
+        .attr("ry", 4);
+
+    // Text for date
+    focus.append("text")
+        .attr("class", "tooltip-date")
+        .attr("x", 18)
+        .attr("y", -2);
+
+    // Text for close price
+    focus.append("text")
+        .attr("x", 18)
+        .attr("y", 18)
+        .text("Close Price:");
+
+    // value for close price
+    focus.append("text")
+        .attr("class", "tooltip-likes")
+        .attr("x", 100)
+        .attr("y", 18);
+
+    // transparent rect for tool tip mouseover
+    svg.append("rect")
+        .attr("class", "overlay")
+        .attr("width", width)
+        .attr("height", height)
+        .on("mouseover", function() { focus.style("display", null); })
+        .on("mouseout", function() { focus.style("display", "none"); })
+        .on("mousemove", mousemove);
+
+    // Tooltip function
+    function mousemove() {
+        var x0 = x.invert(d3.mouse(this)[0]),
+            i = bisectDate(dataFilter, x0, 1),
+            d0 = dataFilter[i - 1],
+            d1 = dataFilter[i],
+            d = x0 - d0.begins_at > d1.begins_at - x0 ? d1 : d0;
+        focus.attr("transform", "translate(" + x(d.begins_at) + "," + y(d.close_price) + ")");
+        focus.select(".tooltip-date").text(dateFormatter(d.begins_at));
+        focus.select(".tooltip-likes").text(formatValue(d.close_price));
+    }
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+    // A function that update the chart
+    function update(selectedGroup) {
+
+      // Create new data with the selection
+      var dataFilter = data.filter(function(d){return d.symbol==selectedGroup});
+
+      // Add Y axis with selected group to get y axis right
+        var y = d3.scaleLinear()
+          .domain([d3.min(dataFilter, function(d) { return d.close_price; }), d3.max(dataFilter, function(d) { return d.close_price; })])
+          .range([ height, 0 ]);
+        svg.append("g")
+          .attr("class", "y_axis")
+          .transition()
+          .duration(1000)
+          .call(d3.axisLeft(y)
+            .tickFormat(d3.format("$.2f")));
+
+      // Give these new data to update line
+      line
+          .datum(dataFilter)
+          .transition()
+          .duration(1000)
+          .attr("d", d3.line()
+            .x(function(d) { return x(d.begins_at) })
+            .y(function(d) { return y(d.close_price) })
+          )
+          .attr("stroke", '#74ec74')
+
+        // Add Y axis for bar chart
+        y_bar
+            .domain([0, d3.max(dataFilter, function(d) { return d.volume + 30000000 }) ]);
+        yAxis
+            .transition()
+            .duration(1000)
+            .attr("transform", "translate(" + width + ",0)")
+            .call(d3.axisRight(y_bar)
+                .tickFormat(d3.format(".2s")));
+
+        // add X axis for bar chart
+        x_bar
+            .domain(dataFilter.map(function(d) { return dateFormatter(d.begins_at); }))
+        xAxis
+            .attr('id', 'x_bar_axis')
+            .call(d3.axisTop(x_bar));
+
+
+        // variable u: map data to existing bars
+        var u = svg.selectAll("rect")
+          .data(dataFilter)
+
+        // update bars
+        u
+          .enter()
+          .append("rect")
+          .merge(u)
+          .transition()
+        .duration(1000)
+            .attr("x", function(d) { return x_bar(dateFormatter(d.begins_at)); })
+            .attr("y", function(d) { return y_bar(d.volume); })
+            .attr("width", x_bar.bandwidth())
+            .attr("height", function(d) { return height - y_bar(d.volume); })
+            .attr("fill", "#777777")
+
+        // transparent rect for tool tip mouseover
+        svg.append("rect")
+            .attr("class", "overlay")
+            .attr("width", width)
+            .attr("height", height)
+            .on("mouseover", function() { focus.style("display", null); })
+            .on("mouseout", function() { focus.style("display", "none"); })
+            .on("mousemove", mousemove);
+
+        // Tooltip function
+        function mousemove() {
+            var x0 = x.invert(d3.mouse(this)[0]),
+            i = bisectDate(dataFilter, x0, 1),
+            d0 = dataFilter[i - 1],
+            d1 = dataFilter[i],
+            d = x0 - d0.begins_at > d1.begins_at - x0 ? d1 : d0;
+            focus.attr("transform", "translate(" + x(d.begins_at) + "," + y(d.close_price) + ")");
+            focus.select(".tooltip-date").text(dateFormatter(d.begins_at));
+            focus.select(".tooltip-likes").text(formatValue(d.close_price));
+            }
+        } //end of updateChart function
+
+    // When the button is changed, run the updateChart function
+    d3.select("#selectButton").on("change", function(d) {
+        // Select y axis by HTML class and remove when selection is changed
+        var old_y_axis = d3.select('.y_axis');
+            old_y_axis.remove();
+        // Select overlay rect by HTML class and remove when selection is changed
+        var old_rect = d3.select('.overlay');
+            old_rect.remove();
+        // recover the option that has been chosen
+        var selectedOption = d3.select(this).property("value")
+        // run the updateChart function with this selected option
+        update(selectedOption)
+    })
+});
